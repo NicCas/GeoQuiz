6d23d67245e116096b56a43863f018dc31ca826e
Nicole C <nicas@sonic.net>
2021-02-11 13:44:26 -0800

    Fully Completed no Challenges Implemented

--- app/src/main/java/com/bignerdranch/android/geoquiz/CheatActivity.kt
+++ app/src/main/java/com/bignerdranch/android/geoquiz/CheatActivity.kt
@@ -1,11 +1,61 @@
 package com.bignerdranch.android.geoquiz
 
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+
+const val EXTRA_ANSWER_SHOWN = "com.bignerdranch.android.geoquiz.answer_shown"
+
+private const val EXTRA_ANSWER_IS_TRUE = "com.bignerdranch.android.geoquiz.answer_is_true"
 
 class CheatActivity : AppCompatActivity() {
+
+    private lateinit var answerTextView: TextView
+    private lateinit var showAnswerButton: Button
+
+
+    private var answerIsTrue = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_cheat)
+
+        //Check what answer is for cheat
+        answerIsTrue = intent.getBooleanExtra(EXTRA_ANSWER_IS_TRUE, false)
+        answerTextView = findViewById(R.id.answer_text_view)
+        showAnswerButton = findViewById(R.id.show_answer_button)
+
+        //Set answer text
+        showAnswerButton.setOnClickListener {
+
+            val answerText = when {
+                answerIsTrue -> R.string.true_button
+                else -> R.string.false_button
+            }
+
+            answerTextView.setText(answerText)
+            setAnswerShownResult(true)
+        }
+    }
+
+    //Send back intent
+    private fun setAnswerShownResult(isAnswerShown: Boolean) {
+        val data = Intent().apply {
+            putExtra(EXTRA_ANSWER_SHOWN, isAnswerShown)
+        }
+        setResult(Activity.RESULT_OK, data)
+    }
+
+    //Better implementation of Intent for Cheat action
+    companion object {
+        fun newIntent(packageContext: Context, answerIsTrue: Boolean): Intent {
+            return Intent(packageContext, CheatActivity::class.java).apply {
+                putExtra(EXTRA_ANSWER_IS_TRUE, answerIsTrue)
+            }
+        }
     }
 }
