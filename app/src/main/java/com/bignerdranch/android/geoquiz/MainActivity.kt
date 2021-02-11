21c489b3a4080eeb9533f20e3d776e91790986f8
Nicole C <nicas@sonic.net>
2021-02-10 22:55:28 -0800

    Add architecture for more Questions - Added next button - Added question strings - added logic for setting button answers and question text

--- app/src/main/java/com/bignerdranch/android/geoquiz/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/geoquiz/MainActivity.kt
@@ -5,20 +5,41 @@ import android.os.Bundle
 import android.view.Gravity
 import android.view.View
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
+    //Declare Buttons and Text
     private lateinit var trueButton: Button
     private lateinit var falseButton: Button
+    private lateinit var nextButton: Button
+    private lateinit var questionTextView: TextView
 
+    //Declare Buttons
+    private val questionBank = listOf(
+            Question(R.string.question_australia, true),
+            Question(R.string.question_oceans, true),
+            Question(R.string.question_mideast, false),
+            Question(R.string.question_africa, false),
+            Question(R.string.question_americas, true),
+            Question(R.string.question_asia, true))
+
+    //Question number
+    private var currentIndex = 0
+
+    //Main
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        //Find ID of buttons and text
         trueButton = findViewById(R.id.true_button)
         falseButton = findViewById(R.id.false_button)
+        nextButton = findViewById(R.id.next_button)
+        questionTextView = findViewById(R.id.question_text_view)
 
+        //Define action of True Button when clicked
         trueButton.setOnClickListener { view: View ->
             Toast.makeText(
                 this,
@@ -30,6 +51,7 @@ class MainActivity : AppCompatActivity() {
                     }
         }
 
+        //Define action of False Button when clicked
         falseButton.setOnClickListener { view: View ->
             Toast.makeText(
                 this,
@@ -40,5 +62,9 @@ class MainActivity : AppCompatActivity() {
                         show()
                     }
         }
+
+        //Set question text
+        val questionTextResId = questionBank[currentIndex].textResId
+        questionTextView.setText(questionTextResId)
     }
 }
