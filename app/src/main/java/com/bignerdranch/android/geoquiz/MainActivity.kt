05b3a76dbbf7d452b582248277b40c4139f1a196
Nicole C <nicas@sonic.net>
2021-02-11 13:23:14 -0800

    Defined Cheat activity in MainActivity.kt

--- app/src/main/java/com/bignerdranch/android/geoquiz/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/geoquiz/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.bignerdranch.android.geoquiz
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -64,7 +65,8 @@ class MainActivity : AppCompatActivity() {
 
         //Define action of Cheat Button when clicked
         cheatButton.setOnClickListener {
-            // Start CheatActivity
+            val intent = Intent(this, CheatActivity::class.java)
+            startActivity(intent)
         }
 
         updateQuestion()
